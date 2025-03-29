ba4b88f6f1cc9133d71125e8c17e8a762a12d556
Guilherme <engguilhermen@gmail.com>
2025-03-29 03:14:33 -0300

    writing protected Routes

--- DjangoRestAppWeb/frontend/src/components/ProtectedRoute.jsx
+++ DjangoRestAppWeb/frontend/src/components/ProtectedRoute.jsx
@@ -4,5 +4,52 @@ import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN} from "../constants";
 
 function ProtectedRoute({children}) {
-    
+    const [isAuthorized, setIsAuthorized] = useState(false);
+    // carregando uma rota protegida
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false));
+    }   , []);
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try{
+            const res = await api.post("/api/auth/refresh", {
+                refresh: refreshToken
+            });
+            if (res.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+        } catch (error) {
+            console.log(error);
+            setIsAuthorized(false);
+
+        }
+    }
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if (!token) {
+            setIsAuthorized(false)
+            return
+        }
+        const decoded = jwtDecode(token)
+        const tokenExpiration = decoded.exp
+        const now = Date.now() / 1000
+        if (tokenExpiration > now) {
+            await refreshToken()
+        } else {
+            setIsAuthorized(false)
+            return
+        }
+    }
+
+    if (isAuthorized == null) {
+        return <div>Loading...</div>
+    }
+
+
+    return isAuthorized ? children : <Navigate to="/login"/>
 }
