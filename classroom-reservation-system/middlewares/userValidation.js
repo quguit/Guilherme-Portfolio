4984b362544162afd12b1ab2e539307b10bc37b1
Guilherme <engguilhermen@gmail.com>
2025-04-17 21:25:09 -0300

    create registration route with middlewares validation and password hash with bcrypt
    Validação feita com espress validation, onde é necessário para garantir a segurança da api, alem da verificação no front, middlewares de validação para registro completo com confirmação e senha com regras

---
+++ classroom-reservation-system/middlewares/userValidation.js
@@ -0,0 +1,41 @@
+const {body, validationResult } = require('express-validator');
+
+exports.validateRegister = [
+    body('name')
+        .notEmpty().withMessage('Nome é obrigatório')
+        .isLength({ min: 3}).withMessage('Nome deve ter pelo menos 3 caracteres'),
+
+    body('email')
+        .notEmpty().withMessage('Email é obrigatório')
+        .isEmail().withMessage('Email inválido'),
+
+    body('password')
+        .notEmpty().withMessage('Senha é obrigatória')
+        .isLength({ min: 8}).withMessage('Senha deve ter pelo menos 8 caracteres')
+        .matches(/[A-Z]/).withMessage('A senha deve pelo menos uma letra maiúscula')
+        .matches(/[0-9]/).withMessage('A senha deve conter pelo menos um número')
+        .matches(/[\W_]/).withMessage('A senha deve conter pelo menos um caractere especial'),
+    body('confirm_password')
+        .notEmpty().withMessage('Confirmação de senha é obrigatória')
+        .custom((value, { req }) => {
+            if(value !== req.body.password) {
+                throw new Error('As senhas não coincidem');
+            }
+            return true;
+        }),
+
+    body('type_user')
+        .notEmpty().withMessage('Tipo de usuário é obrigatório')
+        .isIn(['student', 'teacher', 'servant']).withMessage('Tipo de usuário é obrigatório'),
+
+    body('identification')
+        .notEmpty().withMessage('Identificação é obrigatoria'),
+    
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ erros: errors.array() });
+        }
+        next();
+    }
+];
--- classroom-reservation-system/routes/userRoutes.js
+++ classroom-reservation-system/routes/userRoutes.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const { validateRegister } = require('../middlewares/userValidation');
 
-router.post('/register', UserController.register);
+router.post('/register', validateRegister, UserController.register);
 router.post('/login', UserController.login);
 
 module.exports = router;
