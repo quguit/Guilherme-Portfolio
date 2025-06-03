4b27005f3215e9f41b4935fd7a7da8caa476b4a2
Guilherme <engguilhermen@gmail.com>
2025-06-03 07:02:16 -0300

    Update bookingForm.js

--- classroom-reservation-system/public/js/bookingForm.js
+++ classroom-reservation-system/public/js/bookingForm.js
@@ -1,5 +1,26 @@
 
+
 document.getElementById("bookingRequestForm").addEventListener("submit", function(e) {
     e.preventDefault(); 
-    alert("Reserva solicitada com sucesso!");
+    
+    const bookingData = {
+        room: document.getElementById("room").value,
+        date: document.getElementByid("date").value,
+        startTime: document.getElementById("startTime").value,
+        endTime: document.getElementById("endTime").value,
+        reason: document.getElementById("reason").value.trim(),
+
+        //simulates logged-in user
+        user:JSON.parse(localStorage.getItem("userData"))?.email || "email@exemplo.com" 
+    };
+
+    axios.post("/api/bookings", bookingData)
+    .then(res => {
+      alert("Reserva solicitada com sucesso!");
+      document.getElementById("bookingRequestForm").reset();
+    })
+    .catch(err => {
+      const message = err.response?.data?.error || err.message || "Erro desconhecido.";
+      alert("Erro ao criar reserva: " + message);
+    });
 });
