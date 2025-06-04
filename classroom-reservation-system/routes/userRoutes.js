d5e22f1caf9b2be6a40087054b7d20e62e3b7ed9
Guilherme <engguilhermen@gmail.com>
2025-06-04 06:20:28 -0300

    Update userRoutes.js

--- classroom-reservation-system/routes/userRoutes.js
+++ classroom-reservation-system/routes/userRoutes.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { validateRegister } = require('../middlewares/userValidation');
 const { validateResetPassword } = require('../middlewares/userValidation');
-
+const auth = require('../middlewares/auth'); // Authentication middleware
 
 router.post('/register', validateRegister, userController.register);
 router.post('/login', userController.login);
 router.get('/', userController.list);
+router.get('/profile', auth, userController.profile);
 router.post('/recover', userController.recoverPassword);
 router.post('/reset-password', validateResetPassword, userController.resetPassword);
 module.exports = router;
